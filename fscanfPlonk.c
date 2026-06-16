6b12c2e5ba4c9bba34439d4b5514985f01185b49
ThePotato-UI <lh38821@student.uni-lj.si>
2026-06-16 16:59:48 +0200

    lol

---
+++ fscanfPlonk.c
@@ -0,0 +1,21 @@
+/*
+--FSANF--
+"%d/s/c/ld/f/lf/" -- int, string, char, long, float, double
+"%99x" -- beri max 99 znakov + \0
+"[...]" -- scanset -> beri dokler/kadar
+"[abc]" -- beri SAMO zanke a, b in c
+"[a-zA-z]" -- SAMO črke
+"[^,]" -- vse RAZEN vejice --> '^' pomeni RAZEN
+"[^\n]" -- vse RAZEN '\n'
+"[^,\"]" -- vse RAZEN vejice in ' " '(narekovanja)
+" " -- preskoci whitespace --> \n \t presledke
+"%*d" -- preberi int ampak ga ZAVRZI (* pomeni zavrzi)
+"%*[^,\n ]" -- pozri vse vejice, presledke in newline
+" \"%99[^\"]\"" -- preberi 99 znakov BREZ vejice, presledkov in narekovajev --> .."REKA",.. --> se shrani v pointerju kot REKA
+
+fscanf vrne stevilo uspeno prebranih elementov/argumentov in ne znakov ali EOF ce je konec datoteke
+
+--FREAD-- --> BINARNE datotke
+fread(pointer, velikost_elementa(sizeof(itd.) v bajtih), stevilo_elementov, datoteka)
+fread vrne stevilo prebranih elementov k mora biti <= stevilo_elementov, ce je manj od 1 potem je ALI napaka ALI konec datoteke
+*/
