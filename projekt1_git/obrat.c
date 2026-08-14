8bf46356c26fd8b4ca296a6c87b567a4ea377032
ThePotato-UI <lh38821@student.uni-lj.si>
2026-08-14 19:39:34 +0200

    testiranje

---
+++ projekt1_git/obrat.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+#include "obrat.h"
+
+Vozlisce* iterativno(Vozlisce* zacetek) {
+    // popravite / dopolnite ...
+    return NULL;
+}
+
+Vozlisce* rekurzivno(Vozlisce* zacetek) {
+    // popravite / dopolnite ...
+    return NULL;
+}
+
+#ifndef test
+
+int main() {
+    return 0;
+}
+
+#endif
